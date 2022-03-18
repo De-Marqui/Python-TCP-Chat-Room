3fd20e58f1b81f59bd11e72ffabccd79a49a3816
Victor De Marqui <victor.demarqui@gmail.com>
2022-03-18 02:47:00 -0300

    Update ClientSide.py

--- ClientSide.py
+++ ClientSide.py
@@ -2,8 +2,7 @@ import socket, threading
 
 def send():
     while True:
-        msg = input("\n<You> ")
-        cli_sock.send(msg.encode())
+        cli_sock.send(input('\n').encode())
 
 def receive():
     while True:
@@ -12,50 +11,17 @@ def receive():
         print(f"\n<{str(sen_name)}> {str(data)}")
 
 if __name__ == "__main__":   
-    # socket
     cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # connect
     HOST = '192.168.0.22'
     PORT = 5023
     cli_sock.connect((HOST, PORT))
     
-    print('---- Conectado ao Servidor! ----')
-    uname = input('Enter your name to enter the chat > ')
+    print('[Connected to the server!]')
+    uname = input('Enter your nickname: ')
     cli_sock.send(uname.encode())
 
     thread_send = threading.Thread(target = send)
     thread_send.start()
-
-    thread_receive = threading.Thread(target = receive)
-    thread_receive.start()
-import socket, threading
-
-def send():
-    while True:
-        msg = input('\nMe > ')
-        cli_sock.send(msg.encode())
-
-def receive():
-    while True:
-        sen_name = cli_sock.recv(1024)
-        data = cli_sock.recv(1024)
-        print('\n' + str(sen_name) + ' > ' + str(data))
-
-if __name__ == "__main__":   
-    # socket
-    cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # connect
-    HOST = '192.168.0.22'
-    PORT = 5023
-    cli_sock.connect((HOST, PORT))     
-    print('Connected to remote host...')
-    uname = input('Enter your name to enter the chat > ')
-    cli_sock.send(uname.encode())
-
-    thread_send = threading.Thread(target = send)
-    thread_send.start()
-
     thread_receive = threading.Thread(target = receive)
     thread_receive.start()
